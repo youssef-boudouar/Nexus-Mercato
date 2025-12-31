1d43d7e19e7b8f9b0f786d7b123ca46766262892
ZIIIPPOO <youssefboudouar77@gmail.com>
2025-12-31 16:53:07 +0100

    feat: add CRUD methods to Coach model

--- models/Coach.php
+++ models/Coach.php
@@ -2,5 +2,42 @@
 
 class Coach extends Person 
 {
-  
+
+    public function getAll()
+    {
+        $sql = "SELECT * FROM coaches";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function create()
+    {
+        $sql = "INSERT INTO coaches(name, nationality) VALUES(?, ?)";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$this->name, $this->nationality]);
+    }
+
+    public function update()
+    {
+        $sql = "UPDATE coaches SET name = ?, nationality = ? WHERE id = ?";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$this->name, $this->nationality, $this->id]);
+    }
+    
+    public function findById($id)
+    {
+        $sql = "SELECT * FROM coaches WHERE id = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function delete()
+    {
+        $sql = "DELETE FROM coaches WHERE id = ?";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$this->id]);
+    }
+
 }
