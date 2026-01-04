c387b17798696fd00bf49fdde59316b044f6fa49
ZIIIPPOO <youssefboudouar77@gmail.com>
2026-01-04 08:07:56 +0100

    feat: implement logout pattern

--- auth/logout.php
+++ auth/logout.php
@@ -1,10 +1,5 @@
 <?php
-// TODO: Start session
-
-// TODO: Unset all session variables
-
-// TODO: Destroy the session
-
-// TODO: Redirect to login.php or index.php
-
-?>
+session_start();
+session_unset();
+session_destroy();
+header('Location: ../index.php');
