7457dd6a6ad02aaf107a6050e9ae59ad8e1136b7
ZIIIPPOO <youssefboudouar77@gmail.com>
2026-01-04 16:48:59 +0100

    feat: add contract negotiation forms and termination controls

---
+++ contracts/delete_contracts.php
@@ -0,0 +1,24 @@
+<?php
+// =====================================================
+// DELETE CONTRACT HANDLER
+// =====================================================
+
+// TODO: Start session
+
+// TODO: Check if user is admin (redirect if not)
+
+// TODO: Include database connection
+
+// TODO: Include Contract model
+
+// TODO: Get contract ID from URL (?id=X)
+
+// TODO: Validate ID exists
+
+// TODO: Delete contract from database using model
+
+// TODO: If success: redirect to contracts.php?deleted=success
+
+// TODO: If error: redirect to contracts.php?deleted=error
+
+?>
